fe21c70bd8fda3e3c542e037d94d55b95db2adb7
serhii_potapov <tomato4422@gmail.com>
2020-04-11 23:51:30 +0300

    add history

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Board } from './Board';
 import styled from 'styled-components';
+import { calculateWinner } from 'utils/calculateWinner';
+import { useDidMountEffect } from 'utils/useDidMountEffect';
 
 import 'style/style.css'
 
@@ -13,15 +15,68 @@ const GameInfo = styled.div`
     margin-left: 20px;
 `;
 
-const App = React.memo(() => {
+const NewGame = styled.button`
+	margin-top: 25px;
+`;
 
-		return (
+const App = React.memo(() => {
+	const initialBoard = Array(9).fill(null);
+	const [localHistory, setLocalHistory] = React.useState({initialBoard});
+	const [xIsNext, setXIsNext] = React.useState(true);
+
+	// useDidMountEffect(() => {
+	// 	const history = localHistory;
+	// 	const current = history[history.length - 1];
+	// 	const winner = calculateWinner(current.squares);
+	// 	console.log('%c', 'background: yellow; color: black;', localHistory);
+	// },[localHistory])
+
+	const handleNewGame = () => {
+		console.log('New game start!!!');
+		setXIsNext(true);
+		setLocalHistory(initialBoard)
+	};
+
+
+
+	const status = () => {
+		// const winner = calculateWinner(current.squares) || null;
+		const winner =  null;
+			if(winner) {
+				return `Победил ${winner}`;
+			} else {
+				return `Следующий ход: ${xIsNext ? 'X' : 'O'}`;
+			}
+
+	};
+
+	const handleClick = (i) => {
+		const history = localHistory;
+		const current = localHistory[localHistory.length - 1];
+		const squares = current.squares.slice();
+		if(calculateWinner(squares) || squares[i]) {
+			return undefined;
+		}
+		squares[i] = xIsNext ? 'X' : 'O';
+		history.concat([{
+			squares
+		}])
+		setLocalHistory({
+			history
+		})
+	}
+
+	return (
 			<Game>
 				<div className="game-board">
-					<Board />
+					<Board
+						squares={localHistory[localHistory.length - 1]}
+						onClick={(i) => handleClick(i)}
+					/>
 				</div>
+				<NewGame onClick={handleNewGame}>Начать заново</NewGame>
 				<GameInfo className="game-info">
-					<div>{/* status */}</div>
+					<div>{status()}</div>
 					<ol>{/* TODO */}</ol>
 				</GameInfo>
 			</Game>
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Square } from './Square';
-import { calculateWinner } from 'utils/calculateWinner'
-import { useDidMountEffect } from 'utils/useDidMountEffect'
+import { calculateWinner } from 'utils/calculateWinner';
+import { useDidMountEffect } from 'utils/useDidMountEffect';
 
 const BoardRow = styled.div`
 	:after {
@@ -16,57 +16,21 @@ const Status = styled.div`
 	margin-bottom: 10px;
 `;
 
-const NewGame = styled.button`
-	margin-top: 25px;
-`;
 
-const Board = React.memo(() => {
-	const initialBoard = Array(9).fill(null)
-	const [squares, setSquares] = React.useState(initialBoard);
-	const [xIsNext, setXIsNext] = React.useState(true);
-
-	useDidMountEffect(() => {
-		// calculateWinner(squares)
-	}, [squares]);
-
-
+const Board = React.memo(({
+	squares = [],
+	onClick = () => {}
+}) => {
 	const renderSquare = (i) => {
 		return <Square
 			value={squares[i]}
-			onClick={() => handleClick(i)}
+			onClick={() => onClick(i)}
 		/>;
 	};
 
-	const handleClick = (i) => {
-		console.log('handleClick', i);
-		const arr = squares.slice();
-		if (calculateWinner(arr) || squares[i]) {
-			return undefined;
-		}
-		arr[i] = xIsNext ? 'X' : 'O';
-		console.log('handle click arr: ', arr);
-		setXIsNext(!xIsNext);
-		setSquares(arr)
-	}
-
-	const status = (squares) => {
-		const winner = calculateWinner(squares)
-			if(winner) {
-				return `Победил ${winner}`;
-			} else {
-				return `Следующий ход: ${xIsNext ? 'X' : 'O'}`;
-			}
-
-	}
-
-	const handleNewGame = () => {
-		setXIsNext(true);
-		setSquares(initialBoard)
-	}
-
 	return (
 		<>
-			<Status className="status">{status(squares)}</Status>
+			{/*<Status className="status">{status(squares)}</Status>*/}
 			<BoardRow className="board-row">
 				{renderSquare(0)}
 				{renderSquare(1)}
@@ -82,7 +46,6 @@ const Board = React.memo(() => {
 				{renderSquare(7)}
 				{renderSquare(8)}
 			</BoardRow>
-			<NewGame onClick={handleNewGame}>Начать заново</NewGame>
 		</>
 	);
 });
