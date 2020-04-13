148256ccb5fca816628827c1fa0dd51ca95a72c0
serhii_potapov <tomato4422@gmail.com>
2020-04-13 22:48:37 +0300

    update log history

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import 'style/style.css'
 
 const Game = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-flow: column wrap;
 `;
 
 const GameInfo = styled.div`
-    margin-left: 20px;
+    margin-top: 20px;
 `;
 
 const NewGame = styled.button`
@@ -20,72 +20,6 @@ const NewGame = styled.button`
 `;
 
 const App = React.memo(() => {
-// 	class App extends React.Component {
-// 		constructor(props) {
-// 			super(props);
-// 			this.state = {
-// 				history: [{
-// 					squares: Array(9).fill(null)
-// 				}],
-// 				xIsNext: true
-// 			};
-// 		}
-//
-// 		handleClick(i) {
-// 			const history = this.state.history;
-// 			const current = history[history.length - 1];
-// 			const squares = current.squares.slice();
-// 			if (calculateWinner(squares) || squares[i]) {
-// 				return;
-// 			}
-// 			squares[i] = this.state.xIsNext ? 'X' : 'O';
-// 			this.setState({
-// 				history: history.concat([{
-// 					squares: squares
-// 				}]),
-// 				xIsNext: !this.state.xIsNext,
-// 			});
-// 		}
-//
-// 		render() {
-// 			const history = this.state.history;
-// 			const current = history[history.length - 1];
-// 			const winner = calculateWinner(current.squares);
-//
-// 			const moves = history.map((step, move) => {
-// 				const desc = move ?
-// 					'Go to move #' + move :
-// 					'Go to game start';
-// 				return (
-// 					<li>
-// 						<button onClick={() => this.jumpTo(move)}>{desc}</button>
-// 					</li>
-// 				);
-// 			});
-//
-// 			let status;
-// 			if (winner) {
-// 				status = 'Winner: ' + winner;
-// 			} else {
-// 				status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-// 			}
-//
-// 			return (
-// 				<div className="game">
-// 					<div className="game-board">
-// 						<Board
-// 							squares={current.squares}
-// 							onClick={(i) => this.handleClick(i)}
-// 						/>
-// 					</div>
-// 					<div className="game-info">
-// 						<div>{status}</div>
-// 						<ol>{moves}</ol>
-// 					</div>
-// 				</div>
-// 			);
-// 		}
-// 	}
 
 		const initialBoard = Array(9).fill(null);
 		const [localHistory, setLocalHistory] = React.useState([{squares: initialBoard}]);
@@ -105,31 +39,33 @@ const App = React.memo(() => {
 			setXIsNext(!xIsNext);
 		};
 
-		// useDidMountEffect(() => {
-		// 	const history = localHistory;
-		// 	const current = history[history.length - 1];
-		// 	const winner = calculateWinner(current.squares);
-		// 	console.log('%c', 'background: yellow; color: black;', localHistory);
-		// },[localHistory])
-
 		const handleNewGame = () => {
 			console.log('New game start!!!');
 			setXIsNext(true);
 			setLocalHistory([{squares: initialBoard}])
 		};
 
-
 		const status = () => {
-			// const winner = calculateWinner(current.squares) || null;
-			const winner = null;
+			const winner = calculateWinner(localHistory[localHistory.length - 1].squares);
 			if (winner) {
 				return `Победил ${winner}`;
 			} else {
 				return `Следующий ход: ${xIsNext ? 'X' : 'O'}`;
 			}
-
 		};
 
+		const moves = localHistory.map((step, move) => {
+			const desc = move ?
+				`Go to move # ${move}` :
+				'Go to game start';
+			return (
+				<li>
+					<button
+						onClick={() => console.log('move to ...')}
+					>{desc}</button>
+				</li>
+			)
+		});
 
 		return (
 			<Game>
@@ -139,11 +75,12 @@ const App = React.memo(() => {
 						onClick={(i) => handleClick(i)}
 					/>
 				</div>
-				<NewGame onClick={handleNewGame}>Начать заново</NewGame>
 				<GameInfo className="game-info">
 					<div>{status()}</div>
-					<ol>{/* TODO */}</ol>
+					<NewGame onClick={handleNewGame}>Начать заново</NewGame>
+					<ol>{moves}</ol>
 				</GameInfo>
+
 			</Game>
 		);
 	}
